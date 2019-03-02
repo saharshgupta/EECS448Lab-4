a7952957c51ee345413ddf24d6abcf928273fcff
Saharsh Gupta <s510g881@ku.edu>
2019-03-01 19:31:37 -0600

    Final php file with the required functionality

--- Exercise-1/mtable.php
+++ Exercise-1/mtable.php
@@ -3,12 +3,23 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 function multable(){
-	for($i = 1; $i <=10; $i++){
-		for($j=1; $j<=10;$j++){
-			echo "$i <br> $j";		
-		}	
+	$start = 0;
+	echo "<table>";
+	echo "<tr>";
+	echo "<td>";
+	for($t = 1; $t <=100 ; $t++){
+		echo "<td> $t </td>";
 	}
-	
+	echo "</tr>";
+	for($i = 1; $i <=100; $i++){
+		echo "<tr> <td> $i</td>";
+		for($j=1; $j<=100;$j++){	
+		$temp = $i * $j;	
+		echo "<td> $temp </td>";	
+		}
+		echo "</tr>";	
+	}
+	echo "</table>";
 }
 
 echo multable();
