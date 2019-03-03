52bb2dfe0241ea4e96d1d598841c8d851213148a
Saharsh Gupta <s510g881@ku.edu>
2019-03-03 15:42:06 -0600

    Working bare skeleton

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -1,8 +1,6 @@
 <?php
-$dom = new DOMDocument;
-$q1 = $dom->getElementById("Q1")->value;
 echo "<ul>";
-echo "<li> $q1 " . $_GET["Q1"] . "</li>";
+echo "<li>". $_GET["Question1"] . $_GET["Q1"] . "</li>";
 echo "<li> Option selected for question 2: " . $_GET["Q2"] . "</li>";
 echo "<li> Option selected for question 3: " . $_GET["Q3"] . "</li>";
 echo "<li> Option selected for question 4: " . $_GET["Q4"] . "</li>"; 
