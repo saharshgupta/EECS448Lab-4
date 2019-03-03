3ed6f6de4fc399a3e55d084989486e93715c0f69
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-03 13:03:02 -0600

    Adding information.

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -1,7 +1,7 @@
 <?php
-echo $_GET["Q1"];
-echo $_GET["Q2"];
-echo $_GET["Q3"];
-echo $_GET["Q4"];
-echo $_GET["Q5"];
+echo "Option selected for question 1: " + $_GET["Q1"];
+echo "Option selected for question 2: " + $_GET["Q2"];
+echo "Option selected for question 3: " + $_GET["Q3"];
+echo "Option selected for question 4: " + $_GET["Q4"];
+echo "Option selected for question 5: " + $_GET["Q5"];
 ?>
