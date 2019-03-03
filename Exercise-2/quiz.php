88fd9b1782d87966aeaadbf6ac77fb2a0fae1546
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-03 17:11:17 -0600

    Updated format for display.

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -1,9 +1,9 @@
 <?php
 echo "<ul>";
-echo "<li>". $_GET["Q1"] . $_GET["A1"] . "</li>";
-echo "<li>". $_GET["Q2"] . $_GET["A2"] . "</li>";
-echo "<li>". $_GET["Q3"] . $_GET["A3"] . "</li>";
-echo "<li>". $_GET["Q4"] . $_GET["A4"] . "</li>";
-echo "<li>". $_GET["Q5"] . $_GET["A5"] . "</li>";
+echo "Question: " . $_GET["Q1"] . "<br> Answer Selected: " . $_GET["A1"] . "<br> Correct Answer: " . $_GET["C1"] . "<br>";
+echo "Question: " . $_GET["Q2"] . $_GET["A2"] . "<br>";
+echo "Question: " . $_GET["Q3"] . $_GET["A3"] . "<br>";
+echo "Question: " . $_GET["Q4"] . $_GET["A4"] . "<br>";
+echo "Question: ". $_GET["Q5"] . $_GET["A5"] . "<br>";
 echo "</ul>";
 ?>
