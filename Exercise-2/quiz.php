4c0d9db7ff8a77ba1f76c323cb47df517a016381
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-03 13:12:52 -0600

    Initial basic information.

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -4,5 +4,6 @@ echo "<li> Option selected for question 1: " . $_GET["Q1"] . "</li>";
 echo "<li> Option selected for question 2: " . $_GET["Q2"] . "</li>";
 echo "<li> Option selected for question 3: " . $_GET["Q3"] . "</li>";
 echo "<li> Option selected for question 4: " . $_GET["Q4"] . "</li>"; 
-echo "Option selected for question 5: " . $_GET["Q5"];
+echo "<li> Option selected for question 5: " . $_GET["Q5"] . "</li>";
+echo "</ul>";
 ?>
