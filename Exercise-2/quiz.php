2d2fd95f85035a0ba87dc3d69e90d6f90220a166
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-03 16:55:27 -0600

    Printing questions and the answer selected

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -1,9 +1,9 @@
 <?php
 echo "<ul>";
-echo "<li>". $_GET["Question1"] . $_GET["Q1"] . "</li>";
-echo "<li> Option selected for question 2: " . $_GET["Q2"] . "</li>";
-echo "<li> Option selected for question 3: " . $_GET["Q3"] . "</li>";
-echo "<li> Option selected for question 4: " . $_GET["Q4"] . "</li>"; 
-echo "<li> Option selected for question 5: " . $_GET["Q5"] . "</li>";
+echo "<li>". $_GET["Q1"] . $_GET["A1"] . "</li>";
+echo "<li>". $_GET["Q2"] . $_GET["A2"] . "</li>";
+echo "<li>". $_GET["Q3"] . $_GET["A3"] . "</li>";
+echo "<li>". $_GET["Q4"] . $_GET["A4"] . "</li>";
+echo "<li>". $_GET["Q5"] . $_GET["A5"] . "</li>";
 echo "</ul>";
 ?>
