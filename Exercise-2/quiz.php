f4cb5bc6b9a2b108977b68ca7646538b418b1875
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-03 13:07:03 -0600

    Information format update.

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -1,7 +1,8 @@
 <?php
-echo "Option selected for question 1: " + $_GET["Q1"];
-echo "Option selected for question 2: " + $_GET["Q2"];
-echo "Option selected for question 3: " + $_GET["Q3"];
-echo "Option selected for question 4: " + $_GET["Q4"];
-echo "Option selected for question 5: " + $_GET["Q5"];
+echo "<ul>";
+echo "<li> Option selected for question 1: " . $_GET["Q1"] . "</li>";
+echo "<li> Option selected for question 2: " . $_GET["Q2"] . "</li>";
+echo "<li> Option selected for question 3: " . $_GET["Q3"] . "</li>";
+echo "<li> Option selected for question 4: " . $_GET["Q4"] . "</li>"; 
+echo "Option selected for question 5: " . $_GET["Q5"];
 ?>
