496af128a30942faed6009cd7b4ac66612132931
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-03 13:20:08 -0600

    New format try.

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -1,6 +1,8 @@
 <?php
+$dom = new DomDocument;
+$q1 = $dom->getElementById("Q1").value;
 echo "<ul>";
-echo "<li> Option selected for question 1: " . $_GET["Q1"] . "</li>";
+echo "<li> $q1 " . $_GET["Q1"] . "</li>";
 echo "<li> Option selected for question 2: " . $_GET["Q2"] . "</li>";
 echo "<li> Option selected for question 3: " . $_GET["Q3"] . "</li>";
 echo "<li> Option selected for question 4: " . $_GET["Q4"] . "</li>"; 
