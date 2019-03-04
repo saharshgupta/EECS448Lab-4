675c830b2f99b4bad7c23c9c8b2adcdd7ef5c47f
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-04 16:54:08 -0600

    Added missing semicolon;

--- Exercise-3/customerBackend.php
+++ Exercise-3/customerBackend.php
@@ -3,7 +3,7 @@
         <?php
             $c1 = $_POST["QI1"]*10;
             $c2 = $_POST["QI2"]*50;
-            $c3 = $_POST["QI3"]*500
+            $c3 = $_POST["QI3"]*500;
             echo "Reciept: ";
             echo "<br><b>Item 1: </b><br> Quantity: " . $_POST["QI1"] . ", Price: $10, Total sub: " . $c1;
             echo "<br><b>Item 2: </b><br> Quantity: " . $_POST["QI2"] . ", Price: $50, Total sub: " . $c2;
