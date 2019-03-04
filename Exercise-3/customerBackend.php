647e4ecb579e23a3b9c729cef020ab0947c4457b
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-04 17:18:33 -0600

    Added total cost to the reciept table.

--- Exercise-3/customerBackend.php
+++ Exercise-3/customerBackend.php
@@ -14,14 +14,16 @@
                 $st = "Three day";
             if($so == 50)
                 $st = "Overnight";
+            $tc = $c1 + $c2+ $c3 + $so;
             echo "<h3> Reciept: </h3>";
 
             echo "<table>";
-            echo "<tr><td></td> <th> Quantity </th> <th> Price </th> <th> Total cost </th> </tr>";
+            echo "<tr><td></td> <th> Quantity </th> <th> Price </th> <th> Cost </th> </tr>";
             echo "<tr> <th> Item 1 </th> <td> $q1 </td> <td> 10 </td> <td> $c1 </td> </tr>";
             echo "<tr> <th> Item 2 </th> <td> $q2 </td> <td> 50 </td> <td> $c2 </td> </tr>";
             echo "<tr> <th> Item 3 </th> <td> $q3 </td> <td> 500 </td> <td> $c3 </td> </tr>";
             echo "<tr> <th> Shipping: </th> <td colspan='2'> $st </td> <td> $so </td> </tr>";
+            echo "<tr> <th colspan='3'> Total Cost: </th> <th> $tc </th> </tr>";
             echo "</table>";
         ?>
     </body>
