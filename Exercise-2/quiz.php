be7586c0e4092461e87773a52d1954b00b3ebc97
Saharsh Gupta <s510g881@ku.edu>
2019-03-03 18:44:12 -0600

    Score message added

--- Exercise-2/quiz.php
+++ Exercise-2/quiz.php
@@ -1,16 +1,16 @@
 <?php
-echo "<ul>";
+echo "<html><body><ul>";
 echo "Question: " . $_GET["Q1"] . "<br> Answer Selected: " . $_GET["A1"] . "<br> Correct Answer: Option 1" . "<br>";
 echo "Question: " . $_GET["Q2"] . "<br> Answer Selected: " . $_GET["A2"] . "<br> Correct Answer: Option 1" . "<br>";
 echo "Question: " . $_GET["Q3"] . "<br> Answer Selected: " . $_GET["A3"] . "<br> Correct Answer: Option 1" . "<br>";
 echo "Question: " . $_GET["Q4"] . "<br> Answer Selected: " . $_GET["A4"] . "<br> Correct Answer: Option 1" . "<br>";
 echo "Question: " . $_GET["Q5"] . "<br> Answer Selected: " . $_GET["A5"] . "<br> Correct Answer: Option 1" . "<br>";
 echo "</ul>";
-$a1 = $_REQUEST("A1");
-$a2 = $_REQUEST("A2");
-$a3 = $_REQUEST("A3");
-$a4 = $_REQUEST("A4");
-$a5 = $_REQUEST("A5");
+$a1 = $_GET["A1"];
+$a2 = $_GET["A2"];
+$a3 = $_GET["A3"];
+$a4 = $_GET["A4"];
+$a5 = $_GET["A5"];
 $cor_count = 0;
 if($a1=="Option 1") {
     $cor_count++;
@@ -27,5 +27,6 @@ if($a4=="Option 1"){
 if($a5=="Option 1"){
     $cor_count++;
 }
-echo "<p> The total score is: $cor_count";
+echo "<p> The total score is: $cor_count/5 </p>";
+echo "</body></html>";
 ?>
