7a8235445deae3f1f6acd41762b9996ff7da8c24
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-04 16:52:04 -0600

    Updated some information for the output.

--- Exercise-3/customerBackend.php
+++ Exercise-3/customerBackend.php
@@ -1,10 +1,13 @@
 <html>
     <body>
         <?php
+            $c1 = $_POST["QI1"]*10;
+            $c2 = $_POST["QI2"]*50;
+            $c3 = $_POST["QI3"]*500
             echo "Reciept: ";
-            echo "<b>Item 1: </b><br> Quantity: " . $_POST["QI1"] . "Price: $10";
-            echo "<b>Item 2: </b><br> Quantity: " . $_POST["QI2"] . "Price: $50";
-            echo "<b>Item 3: </b><br> Quantity: " . $_POST["QI3"] . "Price: $500";
+            echo "<br><b>Item 1: </b><br> Quantity: " . $_POST["QI1"] . ", Price: $10, Total sub: " . $c1;
+            echo "<br><b>Item 2: </b><br> Quantity: " . $_POST["QI2"] . ", Price: $50, Total sub: " . $c2;
+            echo "<br><b>Item 3: </b><br> Quantity: " . $_POST["QI3"] . ", Price: $500, Total sub: " . $c3;
         ?>
     </body>
 </html>
